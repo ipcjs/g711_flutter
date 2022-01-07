498bd5ad3c3e6c972279038ff990896b1084c46c
ipcjs <gipcjs@gmail.com>
2022-01-07 20:32:09 +0800

    impl by ffi

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,8 +1,17 @@
+import 'dart:developer';
+import 'package:async/async.dart'
+    show StreamSinkExtensions, StreamSinkTransformer;
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 import 'dart:async';
+import 'package:flutter_sound_platform_interface/flutter_sound_recorder_platform_interface.dart';
 
 import 'package:flutter/services.dart';
+import 'package:flutter_sound/flutter_sound.dart';
 import 'package:g711_flutter/g711_flutter.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,11 +26,52 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   String _platformVersion = 'Unknown';
+  G711Flutter g711 = G711Flutter();
+  FlutterSoundPlayer? _player;
+  FlutterSoundRecorder? _recorder;
 
   @override
   void initState() {
     super.initState();
     initPlatformState();
+    _player = FlutterSoundPlayer()..openAudioSession();
+    _recorder = FlutterSoundRecorder()..openAudioSession();
+  }
+
+  @override
+  void dispose() {
+    _player?.closeAudioSession();
+    _recorder?.closeAudioSession();
+    super.dispose();
+  }
+
+  void _test() async {
+    if (!await Permission.microphone.request().isGranted) {
+      return;
+    }
+    if (_player?.isPlaying == true) {
+      await _player?.stopPlayer();
+      await _recorder?.stopRecorder();
+    } else {
+      await _player?.startPlayerFromStream();
+      _recorder?.startRecorder(
+        codec: Codec.pcm16,
+        audioSource: AudioSource.voice_communication,
+        toStream:
+            _player?.foodSink?.transform(StreamSinkTransformer.fromHandlers(
+          handleData: (food, sink) {
+            if (food is FoodData && food.data != null) {
+              final ulaw = g711.pcm16ToUlaw(food.data!);
+              final pcm16 = g711.ulawToPcm16(ulaw);
+              // assert(food.data == pcm16);
+              log('${food.data}\n$pcm16');
+              sink.add(FoodData(pcm16));
+            }
+          },
+        )),
+      );
+    }
+    setState(() {});
   }
 
   // Platform messages are asynchronous, so we initialize in an async method.
@@ -53,8 +103,15 @@ class _MyAppState extends State<MyApp> {
         appBar: AppBar(
           title: const Text('Plugin example app'),
         ),
-        body: Center(
-          child: Text('Running on: $_platformVersion\n'),
+        body: Column(
+          children: [
+            Text(
+                'Running on: $_platformVersion\n${native_add(1, 2) + native_add_func(3, 4)}'),
+            OutlinedButton(
+              onPressed: _test,
+              child: Text(_player?.isPlaying == true ? 'stop' : 'play'),
+            ),
+          ],
         ),
       ),
     );
--- lib/g711_flutter.dart
+++ lib/g711_flutter.dart
@@ -1,9 +1,76 @@
+// ignore_for_file: non_constant_identifier_names
 
 import 'dart:async';
+import 'dart:ffi';
+import 'dart:io';
+import 'dart:typed_data';
 
+import 'package:ffi/ffi.dart';
 import 'package:flutter/services.dart';
 
+final DynamicLibrary _g711 = Platform.isAndroid
+    ? DynamicLibrary.open('libg711.so')
+    : DynamicLibrary.process();
+
+final int Function(int x, int y) native_add = _g711
+    .lookup<NativeFunction<Int32 Function(Int32, Int32)>>('native_add')
+    .asFunction();
+
+final native_add_func = _g711.lookupFunction<Int32 Function(Int32, Int32),
+    int Function(int x, int y)>('native_add');
+
+final _pcm16_ulaw_tableinit = _g711
+    .lookupFunction<Void Function(), void Function()>('pcm16_ulaw_tableinit');
+
+final _ulaw_pcm16_tableinit = _g711
+    .lookupFunction<Void Function(), void Function()>('ulaw_pcm16_tableinit');
+
+final _ulaw_to_pcm16 = _g711.lookupFunction<
+    Void Function(Int32, Pointer<Uint8>, Pointer<Uint8>),
+    void Function(int, Pointer<Uint8>, Pointer<Uint8>)>('ulaw_to_pcm16');
+final _pcm16_to_ulaw = _g711.lookupFunction<
+    Void Function(Int32, Pointer<Uint8>, Pointer<Uint8>),
+    void Function(int, Pointer<Uint8>, Pointer<Uint8>)>('pcm16_to_ulaw');
+
 class G711Flutter {
+  static var _initiated = false;
+  G711Flutter() {
+    if (!_initiated) {
+      _initiated = true;
+      _pcm16_ulaw_tableinit();
+      _ulaw_pcm16_tableinit();
+    }
+  }
+  Uint8List pcm16ToUlaw(Uint8List pcm16) {
+    final inSize = pcm16.length;
+    final outSize = inSize ~/ 2;
+    final inArray = calloc<Uint8>(inSize);
+    final outArray = calloc<Uint8>(outSize);
+    inArray.asTypedList(pcm16.length).setAll(0, pcm16);
+
+    _pcm16_to_ulaw(inSize, inArray, outArray);
+
+    final out = Uint8List.fromList(outArray.asTypedList(outSize));
+    calloc.free(outArray);
+    calloc.free(inArray);
+    return out;
+  }
+
+  Uint8List ulawToPcm16(Uint8List g711) {
+    final inSize = g711.length;
+    final outSize = inSize * 2;
+    final inArray = calloc<Uint8>(inSize);
+    final outArray = calloc<Uint8>(outSize);
+    inArray.asTypedList(g711.length).setAll(0, g711);
+
+    _ulaw_to_pcm16(inSize, inArray, outArray);
+
+    final out = Uint8List.fromList(outArray.asTypedList(outSize));
+    calloc.free(outArray);
+    calloc.free(inArray);
+    return out;
+  }
+
   static const MethodChannel _channel = MethodChannel('g711_flutter');
 
   static Future<String?> get platformVersion async {
