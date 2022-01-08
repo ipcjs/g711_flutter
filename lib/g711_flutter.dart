6131b166a5f99abae4a2b7e74f6fd39a95bba3a9
ipcjs <gipcjs@gmail.com>
2022-01-08 11:33:51 +0800

    add dart_g711.dart file

---
+++ lib/g711_flutter.dart
@@ -0,0 +1,3 @@
+export 'src/dart_g711.dart';
+export 'src/g711_flutter.dart'
+    show G711Codec, NativeG711Codec, native_add, native_add_func;
---
+++ lib/src/dart_g711.dart
@@ -0,0 +1,20 @@
+import 'dart:typed_data';
+
+import 'g711_flutter.dart';
+
+/// Created by ipcjs on 2022/1/8.
+class DartG711Codec implements G711Codec {
+  const DartG711Codec();
+
+  @override
+  Uint8List pcm16ToUlaw(Uint8List pcm16) {
+    // TODO: implement pcm16ToUlaw
+    throw UnimplementedError();
+  }
+
+  @override
+  Uint8List ulawToPcm16(Uint8List g711) {
+    // TODO: implement ulawToPcm16
+    throw UnimplementedError();
+  }
+}
